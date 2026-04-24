e10a3d815563b8a5f3d36e76e0e86fb046909bcf
Ganjar Hadiatna <mdl.ganjarhadiatna@gmail.com>
2026-04-24 23:12:33 +0700

    style(transaction): Simplify ShiftStatus component layout
    - Remove unnecessary wrapper div and flex container
    - Flatten component structure for cleaner markup
    - Improve readability by removing extra nesting levels

--- src/modules/transaction/components/ShiftStatus.vue
+++ src/modules/transaction/components/ShiftStatus.vue
@@ -1,28 +1,26 @@
 <template>
-  <div class="w-full flex flex-col gap-4">
-    <!-- Warning Shift -->
-    <Message
-      v-if="currentShift.status === ShiftStatus.CLOSED"
-      severity="warn"
-      icon="pi pi-info-circle"
-    >
-      <b>Shift is closed.</b><br>
-      <span class="text-sm">
-        Please open new shit to create transactions.
-      </span>
-    </Message>
+  <!-- Warning Shift -->
+  <Message
+    v-if="currentShift.status === ShiftStatus.CLOSED"
+    severity="warn"
+    icon="pi pi-info-circle"
+  >
+    <b>Shift is closed.</b><br>
+    <span class="text-sm">
+      Please open new shit to create transactions.
+    </span>
+  </Message>
 
-    <Message
-      v-if="!isUserInShift && currentShift.status === ShiftStatus.OPEN"
-      severity="info"
-      icon="pi pi-info-circle"
-    >
-      <b>You're not shift participant.</b><br>
-      <span class="text-sm">
-        To create the transactions, you have to be a part of the shift.
-      </span>
-    </Message>
-  </div>
+  <Message
+    v-if="!isUserInShift && currentShift.status === ShiftStatus.OPEN"
+    severity="info"
+    icon="pi pi-info-circle"
+  >
+    <b>You're not shift participant.</b><br>
+    <span class="text-sm">
+      To create the transactions, you have to be a part of the shift.
+    </span>
+  </Message>
 </template>
 
 <script setup lang="ts">
